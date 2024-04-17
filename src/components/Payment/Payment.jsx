b966ef385454b2e4eb40b2da20d5db5ffe703adb
Smriti Ghale <117796038+21040035@users.noreply.github.com>
2024-04-17 15:46:10 +0545

    Update Payment.jsx
    Co-Authored-By: smriti <809276+smriti@users.noreply.github.com>

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -9,7 +9,6 @@ import {
   useStripe,
   useElements,
 } from "@stripe/react-stripe-js";
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import { server } from "../../server";
@@ -65,12 +64,12 @@ const Payment = () => {
       let paymentInfo = payer;
 
       if (paymentInfo !== undefined) {
-        paypalPaymentHandler(paymentInfo);
+        esewaPaymentHandler(paymentInfo);
       }
     });
   };
 
-  const paypalPaymentHandler = async (paymentInfo) => {
+  const esewaPaymentHandler = async (paymentInfo) => {
     const config = {
       headers: {
         "Content-Type": "application/json",
@@ -80,7 +79,7 @@ const Payment = () => {
     order.paymentInfo = {
       id: paymentInfo.payer_id,
       status: "succeeded",
-      type: "Paypal",
+      type: "Esewa",
     };
 
     await axios
@@ -321,7 +320,7 @@ const PaymentInfo = ({
       </div>
 
       <br />
-      {/* paypal payment */}
+      {/* esewa payment */}
       <div>
         <div className="flex w-full pb-5 border-b mb-2">
           <div
@@ -333,44 +332,22 @@ const PaymentInfo = ({
             ) : null}
           </div>
           <h4 className="text-[18px] pl-2 font-[600] text-[#000000b1]">
-            Pay with Paypal
+            Pay with Esewa
           </h4>
         </div>
 
         {/* pay with payement */}
         {select === 2 ? (
           <div className="w-full flex border-b">
+            <a href ="https://merchant.esewa.com.np/auth/login"> 
             <div
               className={`${styles.button} !bg-[#f63b60] text-white h-[45px] rounded-[5px] cursor-pointer text-[18px] font-[600]`}
               onClick={() => setOpen(true)}
             >
               Pay Now
             </div>
-            {open && (
-              <div className="w-full fixed top-0 left-0 bg-[#00000039] h-screen flex items-center justify-center z-[99999]">
-                <div className="w-full 800px:w-[40%] h-screen 800px:h-[80vh] bg-white rounded-[5px] shadow flex flex-col justify-center p-8 relative overflow-y-scroll">
-                  <div className="w-full flex justify-end p-3">
-                    <RxCross1
-                      size={30}
-                      className="cursor-pointer absolute top-3 right-3"
-                      onClick={() => setOpen(false)}
-                    />
-                  </div>
-                    <PayPalScriptProvider
-                      options={{
-                        "client-id":
-                          "Aczac4Ry9_QA1t4c7TKH9UusH3RTe6onyICPoCToHG10kjlNdI-qwobbW9JAHzaRQwFMn2-k660853jn",
-                      }}
-                    >
-                      <PayPalButtons
-                        style={{ layout: "vertical" }}
-                        onApprove={onApprove}
-                        createOrder={createOrder}
-                      />
-                    </PayPalScriptProvider>
-                </div>
-              </div>
-            )}
+            </a>
+            
           </div>
         ) : null}
       </div>
